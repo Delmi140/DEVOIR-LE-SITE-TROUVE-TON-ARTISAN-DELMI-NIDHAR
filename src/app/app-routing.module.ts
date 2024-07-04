22694bb27239431dd3c82bf8638583f30a7f1931
Delmi140 <156790899+Delmi140@users.noreply.github.com>
2024-07-04 19:17:51 +0200

    Add Pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,17 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { HomeComponent } from './home/home.component';
+import { BuildingComponent } from './building/building.component';
+import { ServicesComponent } from './services/services.component';
+import { ManufacturingComponent } from './manufacturing/manufacturing.component';
+import { FoodComponent } from './food/food.component';
 
 const routes: Routes = [
-  {path: "" , component: HomeComponent }
+  {path: "" , component: HomeComponent },
+  {path: "building", component:BuildingComponent },
+  {path: "services", component: ServicesComponent },
+  {path: "manufacturing", component: ManufacturingComponent },
+  {path: "food", component: FoodComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,21 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { BuildingComponent } from './building/building.component';
+import { ServicesComponent } from './services/services.component';
+import { ManufacturingComponent } from './manufacturing/manufacturing.component';
+import { FoodComponent } from './food/food.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    BuildingComponent,
+    ServicesComponent,
+    ManufacturingComponent,
+    FoodComponent
   ],
   imports: [
     BrowserModule,
