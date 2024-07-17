f752a3acd7f20df48498088db314d6bd3a9b6aba
Delmi140 <156790899+Delmi140@users.noreply.github.com>
2024-07-17 18:53:10 +0200

    Add Scss

--- src/app/filter-search.pipe.ts
+++ src/app/filter-search.pipe.ts
@@ -7,7 +7,7 @@ export class FilterSearchPipe  implements PipeTransform {
 
   transform(value: any, searchfilter: any): any {
     return value.filter((e:any) =>{
-      return e.name.indexOf(searchfilter) > -1;
+      return e.name.toLowerCase().indexOf(searchfilter) > -1;
 
     })
   }
